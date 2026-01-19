810a4f71b9cb6a0a124266aa67eb1600f41bb1f6
tan <tanmayrajkrishna@gmail.com>
2026-01-20 03:55:35 +0530

    fix a bug with typing

--- scripts/manager.gd
+++ scripts/manager.gd
@@ -8,42 +8,41 @@ var current_pos: int
 func _input(event: InputEvent) -> void:
 	if event is not InputEventKey or not event.is_pressed():
 		return
+		
+	var key = (event as InputEventKey).as_text_key_label().to_lower()
+	if key.length() != 1:
+		return
 	
 	if not selected_word:
 		for word in present_words:
-			if word[0].to_lower() == (event as InputEventKey).as_text_key_label().to_lower():
+			if word[0].to_lower() == key:
 				selected_word = word
 				current_pos = 1
 				update_caption(current_pos)
-				print([selected_word, current_pos])
 				return
 	else:
-		if selected_word[current_pos].to_lower() == (event as InputEventKey).as_text_key_label().to_lower():
+		if selected_word[current_pos].to_lower() == key:
 			if current_pos == (len(selected_word) - 1):
 				current_pos += 1
 				update_caption(current_pos)
-				print([selected_word, current_pos])
 				slime_gansta(selected_word)
+				present_words.remove_at(present_words.find(selected_word))
 				selected_word = ""
 				current_pos = 0
 			else:
 				current_pos += 1
 				update_caption(current_pos)
-				print([selected_word, current_pos])
-		else:
-			print("wrong sequence")
 			
 func update_caption(pos: int):
 	for child in get_children():
 		if not child.is_in_group("gang"):
 			continue
 		if child.word.to_lower() == selected_word.to_lower():
-			child.set_caption(current_pos)
+			child.set_caption(pos)
 			
 func slime_gansta(word: String):
 	for child in get_children():
-		print(child.name)
 		if not child.is_in_group("gang"):
 			continue
-		if child.word.to_lower() == selected_word.to_lower():
+		if child.word.to_lower() == word.to_lower():
 			child.call_deferred("queue_free")
